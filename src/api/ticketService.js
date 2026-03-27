9bc36fa4297261f8ba8fdbabfc55510b2a2f38e4
jp1593 <juanpablo08082002@hotmail.com>
2026-03-27 13:05:02 -0600

    feat: add api for ticket creation and catalog information

--- src/api/ticketService.js
+++ src/api/ticketService.js
@@ -1,22 +1,32 @@
-import axios from 'axios';
+import axios from "axios";
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000/api', 
+  baseURL: "http://localhost:3000/api",
 });
 
 // Endpoints
-export const getTickets = (page = 1, limit = 10) => 
+// Get Tickets Information
+export const getTickets = (page = 1, limit = 10) =>
   api.get(`/tickets?page=${page}&limit=${limit}`);
 
-export const getTicketById = (id) => 
-  api.get(`/tickets/${id}`);
+// Get Individual Ticket Information
+export const getTicketById = (id) => api.get(`/tickets/${id}`);
 
-export const getWeeklySummary = (year, week) => 
+// Get PaymentSummary Information
+export const getWeeklySummary = (year, week) =>
   api.get(`/payments/summary?year=${year}&week=${week}`);
 
-export const createTicket = (data) => 
-  api.post('/tickets', data);
+// Create Ticket
+export const createTicket = (data) => api.post("/tickets", data);
 
+// Get Data For Dashboard
 export const getDashboardStats = (year, week) => {
   return api.get(`/payments/dashboard-stats`, { params: { year, week } });
 };
+
+// Get Information From Bussiness - Catalogs
+export const getSuppliers = () => api.get("/suppliers");
+
+export const getLands = () => api.get("/lands");
+
+export const getProducts = () => api.get("/products");
